2ce80865c61afd14fc96aa9dee0cdd7bd8530b1f
Jamal Frank <jamaljfrank@gmail.com>
2020-04-09 11:38:24 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,18 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: 'ingredient 1')
+    @recipe.ingredients.build(name: 'ingredient 2')
   end
 
   def create
+    @recipe  = Recipe.create(recipe_params)
+    redirect_to recipes_path
+  end
+  
+  private
+ 
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredient_attributes: [:name, :quantity])
   end
 end
